46a26c010eb4935d71d7cd016cf5a504aab91339
Pawel Gedlek <pawgedlek@gmail.com>
2025-01-25 16:48:59 +0100

    Init repo

---
+++ external-system/src/test/java/com/pgedlek/external_system/ExternalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgedlek.external_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
