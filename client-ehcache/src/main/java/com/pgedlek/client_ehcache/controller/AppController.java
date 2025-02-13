d148606e56c0a6fdec355bb865dd52a59c7f4b97
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-13 21:04:48 +0100

    Add Create and update to ehcache

--- client-ehcache/src/main/java/com/pgedlek/client_ehcache/controller/AppController.java
+++ client-ehcache/src/main/java/com/pgedlek/client_ehcache/controller/AppController.java
@@ -4,17 +4,13 @@ import com.pgedlek.client_ehcache.model.Profile;
 import com.pgedlek.client_ehcache.service.AppService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
 import java.util.Optional;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/api")
@@ -30,6 +26,24 @@ public class AppController {
         return profile.isPresent()
                 ? new ResponseEntity<>(profile, OK)
                 : new ResponseEntity<>("Cannot find profile with ID " + profileId, NOT_FOUND);
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<?> addProfile(@RequestBody Profile profile) {
+        Optional<Profile> createdProfile = appService.addProfile(profile);
+
+        return createdProfile.isPresent()
+                ? new ResponseEntity<>(createdProfile, CREATED)
+                : new ResponseEntity<>("Cannot create profile", INTERNAL_SERVER_ERROR);
+    }
+
+    @PutMapping("/profile/{profileId}")
+    public ResponseEntity<?> updateProfile(@PathVariable Long profileId, @RequestBody Profile profile) {
+        Optional<Profile> updatedProfile = appService.updateProfile(profileId, profile);
+
+        return updatedProfile.isPresent()
+                ? new ResponseEntity<>(updatedProfile, OK)
+                : new ResponseEntity<>("Cannot update profile", INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping(value = "/profile/clear-cache")
--- client-ehcache/src/main/java/com/pgedlek/client_ehcache/service/AppService.java
+++ client-ehcache/src/main/java/com/pgedlek/client_ehcache/service/AppService.java
@@ -2,7 +2,10 @@ package com.pgedlek.client_ehcache.service;
 
 import com.pgedlek.client_ehcache.model.Profile;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,12 +15,37 @@ import java.util.Optional;
 @Slf4j
 @Service
 public class AppService {
-    private static final String URL = "http://localhost:8888/external-system/profile/";
+    private static final String URL = "http://localhost:8888/external-system/profile";
 
     @Cacheable(value = "profile", key = "#profileId")
     public Optional<Profile> getProfile(Long profileId) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Profile> response = restTemplate.getForEntity(URL + profileId, Profile.class);
+        ResponseEntity<Profile> response = restTemplate.getForEntity(URL + "/" + profileId, Profile.class);
+
+        if (response.getStatusCode().is2xxSuccessful() && response.hasBody()) {
+            return Optional.of(response.getBody());
+        }
+
+        return Optional.empty();
+    }
+
+    public Optional<Profile> addProfile(Profile profile) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Profile> response = restTemplate.exchange(URL, HttpMethod.POST, new HttpEntity<Profile>(profile), Profile.class);
+
+        if (response.getStatusCode().is2xxSuccessful() && response.hasBody()) {
+            Profile createdProfile = response.getBody();
+            return Optional.of(createdProfile);
+        }
+
+        return Optional.empty();
+    }
+
+    @CachePut(value = "profile", key = "#profileId")
+    public Optional<Profile> updateProfile(Long profileId, Profile profile) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Profile> response = restTemplate.exchange(URL + "/" + profileId, HttpMethod.PUT, new HttpEntity<Profile>(profile), Profile.class);
 
         if (response.getStatusCode().is2xxSuccessful() && response.hasBody()) {
             return Optional.of(response.getBody());
