7fef41d9b4b3fec591bd41520b6306a55a97e628
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-13 22:07:16 +0100

    Update default spring cache implementation

--- client-spring-cache/src/main/java/com/pgedlek/client_spring_cache/controller/AppController.java
+++ client-spring-cache/src/main/java/com/pgedlek/client_spring_cache/controller/AppController.java
@@ -1,13 +1,16 @@
 package com.pgedlek.client_spring_cache.controller;
 
+import com.pgedlek.client_spring_cache.model.Profile;
 import com.pgedlek.client_spring_cache.service.AppService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RestController
@@ -19,6 +22,20 @@ public class AppController {
     @GetMapping(value = "/profile/{profileId}")
     public Mono<?> getProfile(@PathVariable Long profileId) {
         return appService.getProfile(profileId);
+    }
+
+    @PostMapping("/profile")
+    public Mono<ResponseEntity<Profile>> addProfile(@RequestBody Profile profile) {
+        return appService.addProfile(profile)
+                .map(p -> new ResponseEntity<>(p, CREATED))
+                .switchIfEmpty(Mono.just(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)));
+    }
+
+    @PutMapping("/profile/{profileId}")
+    public Mono<ResponseEntity<Profile>> updateProfile(@PathVariable Long profileId, @RequestBody Profile profile) {
+        return appService.updateProfile(profileId, profile)
+                .map(p -> new ResponseEntity<>(p, OK))
+                .switchIfEmpty(Mono.just(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)));
     }
 
     @GetMapping(value = "/profile/clear-cache")
--- client-spring-cache/src/main/java/com/pgedlek/client_spring_cache/service/AppService.java
+++ client-spring-cache/src/main/java/com/pgedlek/client_spring_cache/service/AppService.java
@@ -1,21 +1,29 @@
 package com.pgedlek.client_spring_cache.service;
 
 import com.pgedlek.client_spring_cache.model.Profile;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class AppService {
+    private final CacheManager cacheManager;
+
     private static final WebClient webClient = WebClient.builder()
             .baseUrl("http://localhost:8888/external-system")
             .build();
 
-    @Cacheable(value = "profiles")
+    @Cacheable(value = "profile", key = "#profileId")
     public Mono<?> getProfile(Long profileId) {
         return webClient.get()
                 .uri("/profile/{profileId}", profileId)
@@ -27,12 +35,41 @@ public class AppService {
                 });
     }
 
-    @CacheEvict(value = "profiles")
+    public Mono<Profile> addProfile(Profile profile) {
+        return webClient.post()
+                .uri("/profile", profile)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(profile))
+                .retrieve()
+                .bodyToMono(Profile.class)
+                .doOnNext(createdProfile -> cacheManager.getCache("profile")
+                        .put(createdProfile.profileId(), createdProfile))
+                .onErrorResume(e -> {
+                    log.error("Error adding profile: " + e.getMessage());
+                    return Mono.empty();
+                });
+    }
+
+    @CachePut(value = "profile", key = "#profileId")
+    public Mono<Profile> updateProfile(Long profileId, Profile profile) {
+        return webClient.put()
+                .uri("/profile/{profileId}", profileId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(profile))
+                .retrieve()
+                .bodyToMono(Profile.class)
+                .onErrorResume(e -> {
+                    log.error("Error updating profile: " + e.getMessage());
+                    return Mono.empty();
+                });
+    }
+
+    @CacheEvict(value = "profile", allEntries = true)
     public void clearAllCaches() {
         log.info("Removed all caches");
     }
 
-    @CacheEvict(value = "profiles", key = "#profileId")
+    @CacheEvict(value = "profile", key = "#profileId")
     public void clearSpecificKey(Long profileId) {
         log.info("Remove profile with ID {} from cache", profileId);
     }
